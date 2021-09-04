58ec70f819b504e9c06a19247b9f4faf814c1021
Szymon Burak <sajmon0031@gmail.com>
2021-09-04 14:14:49 +0200

    Fix bug

--- server/databaseConnection.js
+++ server/databaseConnection.js
@@ -3,15 +3,8 @@ const mysql = require("mysql");
 const con = mysql.createConnection({
     host: "s124.cyber-folks.pl",
     user: "dakwmndatt_brunchbox",
-    password: "Brunch#Box!xyzSkyl@123",
+    password: "***** ***",
     database: "dakwmndatt_brunchbox"
 });
-
-// const con = mysql.createConnection({
-//     host: "18421_brunchbox.skylo-pl.atthost24.pl",
-//     user: "18421_brunchbox",
-//     password: "SwinkaPeppa-31",
-//     database: "18421_brunchbox"
-// });
 
 module.exports = con;
